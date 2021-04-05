278c66f3da479560de8115d15f540af6ba6c99bf
pwd0kernel <pwd0kernel@gmail.com>
2021-04-04 22:18:21 -0230

    Update main.cs

--- main/main.cs
+++ main/main.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Net;
 
-public static CloudScriptScraper {
-    public static void Main(){
-        Console.Write("[Code]: ");
-        string code = Console.ReadLine();
+namespace CloudScriptScraper
+{
+    public static Program {
+        public static void Main(){
+            Console.Write("[Code]: ");
+            string code = Console.ReadLine();
 
-        using (var client = new WebClient()){
-            var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
-            byte[] src = Convert.FromBase64String(data);
-            string source = Encoding.UTF8.GetString(src);
+            using (var client = new WebClient()){
+                var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
+                byte[] src = Convert.FromBase64String(data);
+                string source = Encoding.UTF8.GetString(src);
 
-            Console.WriteLine(source);
+                Console.WriteLine(source);
+            }
         }
     }
 }
