048853e877e1b598b7d247fd776b96dd0e457698
pwd0kernel <pwd0kernel@gmail.com>
2021-04-05 09:29:20 -0230

    Update Program.cs

--- main/Program.cs
+++ main/Program.cs
@@ -15,10 +15,8 @@ namespace CloudScriptScraper
 
                 using (var client = new WebClient()){
                     var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
-                    byte[] src = Convert.FromBase64String(data);
-                    string source = Encoding.UTF8.GetString(src);
 
-                    Console.WriteLine(source);
+                    Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(data)));
                 }
                 
             }
