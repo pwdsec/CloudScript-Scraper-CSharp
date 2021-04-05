d852bb42fe914973bcd5766218effd3b2572ca36
pwd0kernel <pwd0kernel@gmail.com>
2021-04-04 22:37:55 -0230

    Changes

---
+++ main/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace CloudScriptScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true){
+                Console.Write("[Code]: ");
+                string code = Console.ReadLine();
+
+                using (var client = new WebClient()){
+                    var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
+                    byte[] src = Convert.FromBase64String(data);
+                    string source = Encoding.UTF8.GetString(src);
+
+                    Console.WriteLine(source);
+                }
+            }
+        }
+    }
+}
