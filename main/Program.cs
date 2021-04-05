95ced151af971e350de4c98f5892e27a31904c82
pwd0kernel <pwd0kernel@gmail.com>
2021-04-05 10:07:17 -0230

    Update Program.cs

--- main/Program.cs
+++ main/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Net;
 using System.Text;
+using System.IO;
+/*
+*   Private as of: 4/5/2021
+*   Made by pwd0kernel - pwdsec
+*/
 
 namespace CloudScriptScraper
 {
@@ -8,6 +13,9 @@ namespace CloudScriptScraper
     {
         static void Main(string[] args)
         {
+            if (!Directory.Exists("Scripts"))
+                Directory.CreateDirectory("Scripts");
+
             while (true) {
                 Console.Write("[Code]: ");
                 string code = Console.ReadLine();
@@ -16,6 +24,7 @@ namespace CloudScriptScraper
                     var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
 
                     Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(data)));
+                    File.WriteAllText($"Scripts\\{code}_src.lua", Encoding.UTF8.GetString(Convert.FromBase64String(data)));
                     Console.WriteLine($"CloudScript Code: cloudscript({code})()");
                 } 
             }
