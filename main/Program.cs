57df73b5279a6f862d5526a38a81340a1845a00a
pwd0kernel <pwd0kernel@gmail.com>
2021-04-09 19:35:56 -0230

    Update Program.cs

--- main/Program.cs
+++ main/Program.cs
@@ -11,7 +11,8 @@ using System.IO;
 *
 *   Last edited: 4/9/2021 
 *   ChangeLogs:
-*       [4/9/2021] - changed a bit the stuff.
+*       [4/9/2021] - Formated the code.
+*       [4/9/2021] - Changed a bit the stuff.
 *       [4/9/2021] - Shitty {Not Found} handler.
 *       [4/9/2021] - Made it work with { cloudscript(470029)() } it will just remove cloudscript()()
 *       [x/x/xxxx] - Bruteforce.
@@ -22,20 +23,26 @@ namespace CloudScriptScraper
 {
     class Program
     {
-        static void BruceForce(int num) {
+        static void BruceForce(int num)
+        {
             // this can take forever, so just go here: https://lynx.rip/dashboard/home/cloudscripts/storage/
             // you can also use threading so it goes way faster lol, i juts didn't do it (maybe later?)
-            for (int i = 1; i <= num; i = i + 1) { // original [num]{1000000}
-                try {
-                    using (var client = new WebClient()) {
-                        var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{i}_src.lua"); 
+            for (int i = 1; i <= num; i = i + 1)
+            { // original [num]{1000000}
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{i}_src.lua");
                         Console.WriteLine(data);
                     }
-                } catch(Exception) { Console.WriteLine($"Error: {i}"); }
+                }
+                catch (Exception) { Console.WriteLine($"Error: {i}"); }
             }
         }
 
-        static string replace_cs(string data) {
+        static string replace_cs(string data)
+        {
             data = data.Replace("cloudscript", "");
             data = data.Replace("(", "");
             data = data.Replace(")", "");
@@ -49,34 +56,43 @@ namespace CloudScriptScraper
         static void Main()
         {
             Console.WriteLine("Commands: \n brute - BruteForce Scripts\n    help - [show how to use etc]");
-            if (!Directory.Exists("Scripts")) {
+            if (!Directory.Exists("Scripts"))
+            {
                 Directory.CreateDirectory("Scripts");
                 Console.WriteLine("\"Scripts\" Folder created.");
             }
 
-            while (true) {
+            while (true)
+            {
                 Console.Write("[Code/Cloudscript]: ");
                 string code = Console.ReadLine();
 
                 // there are alternative ways of doing replace
                 code = replace_cs(code);
 
-                if (code == "brute") { // low-key useless
+                if (code == "brute")
+                { // low-key useless
                     Console.Write("[Number]: ");
                     BruceForce(int.Parse(Console.ReadLine())); // original [num]{1000000}
-                } else if (code == "help") {
+                }
+                else if (code == "help")
+                {
                     Console.WriteLine("Go on lynx cloudscript page and copy a script.\nThen paste it here and it will show the full script.");
-                } else {
-                    using (var client = new WebClient()) {
+                }
+                else
+                {
+                    using (var client = new WebClient())
+                    {
                         var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
-                        if (!data.Contains("Not Found")) { // shity detector
+                        if (!data.Contains("Not Found"))
+                        { // shity detector
                             Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(data)));
                             File.WriteAllText($"Scripts\\{code}_src.lua", Encoding.UTF8.GetString(Convert.FromBase64String(data)));
                             Console.WriteLine($"CloudScript Code: cloudscript({code})()");
                         }
-                        else{ Console.WriteLine("This script does not exists"); }
+                        else { Console.WriteLine("This script does not exists"); }
                     }
-                } 
+                }
             }
         }
     }
