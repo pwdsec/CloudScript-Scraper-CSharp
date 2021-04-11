0d89597b28b0d2d9ec2284d83ba2339798420c23
pwd0kernel <pwd0kernel@gmail.com>
2021-04-10 22:32:07 -0230

    Update Program.cs

--- main/Program.cs
+++ main/Program.cs
@@ -33,18 +33,19 @@ namespace CloudScriptScraper
 {
     class Program
     {
-        static void BruceForce(int num)
+        static void BruceForce()
         {
             /*this can take forever, so just go here: https://lynx.rip/dashboard/home/cloudscripts/storage/
             you can also use threading so it goes way faster lol, i juts didn't do it (maybe later?)*/
-            for (int i = 1; i <= num; i = i + 1) // original [num]{1000000}
+            Console.Write("[Number]: ");
+            for (int i = 1; i <= int.Parse(Console.ReadLine()); i = i + 1) // original [num]{1000000}
             {
                 try
                 {
                     using (var client = new WebClient())
                     {
                         var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{i}_src.lua");
-                        if (!data.Contains("Not Found")) { Console.WriteLine(i); }
+                        if (!data.Contains("Not Found")) Console.WriteLine(i);
                     }
                 }
                 catch (Exception) { Console.WriteLine($"Error: {i}"); }
@@ -60,7 +61,7 @@ namespace CloudScriptScraper
                 data = data.Replace(")", "");
                 return data;
             }
-            else { return data; }
+            else return data;
         }
 
         /*
@@ -73,37 +74,22 @@ namespace CloudScriptScraper
         static void Main()
         {
             Console.WriteLine("Commands: \n brute - BruteForce Scripts\n    help - [show how to use etc]\n  exit - closes the app");
-            if (!Directory.Exists("Scripts"))
-            {
-                Directory.CreateDirectory("Scripts");
-                Console.WriteLine("\"Scripts\" Folder created.");
-            }
+            if (!Directory.Exists("Scripts")) Directory.CreateDirectory("Scripts");
 
             while (true)
             {
                 Console.Write("[Code/Cloudscript]: ");
                 string code = Console.ReadLine();
 
-                // there are alternative ways of doing replace
-                code = replace_cs(code);
-
-                if (code == "brute") // low-key useless
-                {
-                    Console.Write("[Number]: ");
-                    BruceForce(int.Parse(Console.ReadLine())); // original [num]{1000000}
-                }
-                else if (code == "help")
-                {
-                    Console.WriteLine("Go on lynx cloudscript page and copy a script.\nThen paste it here and it will show the full script.");
-                }
-                else if (code == "exit")
-                {
-                    Environment.Exit(0);
-                }
+                if (code == "brute") BruceForce(); // original [num]{1000000} | lowkey useless
+                else if (code == "help") Console.WriteLine("Go on lynx cloudscript page and copy a script.\nThen paste it here and it will show the full script.");
+                else if (code == "exit") Environment.Exit(0);
                 else
                 {
                     using (var client = new WebClient())
                     {
+                        // there are alternative ways of doing replace
+                        code = replace_cs(code);
                         var data = client.DownloadString($"http://lynx.rip/dashboard/home/cloudscripts/storage/{code}_src.lua");
                         if (!data.Contains("Not Found")) // shity detector
                         {
@@ -111,7 +97,7 @@ namespace CloudScriptScraper
                             File.WriteAllText($"Scripts\\{code}_src.lua", Encoding.UTF8.GetString(Convert.FromBase64String(data)));
                             Console.WriteLine($"CloudScript Code: cloudscript({code})()");
                         }
-                        else { Console.WriteLine("This script does not exists"); }
+                        else Console.WriteLine("This script does not exists");
                     }
                 }
             }
